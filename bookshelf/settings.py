63934c0dc24ee8b75848fec583a0ea4c5365fff6
j-max <jmax.barry@gmail.com>
2019-07-01 18:22:55 -0500

    changed debug to false

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')aaw=leh$a0dzt)wfyl6etu-p#7(%62y5q0q=ci*7*z8l7tn$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
