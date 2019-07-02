367119a9453f92ff00bcf18b5c12a0f009764c60
j-max <jmax.barry@gmail.com>
2019-07-02 11:06:32 -0500

    first view datapoints showing up on bookshelf

--- books/views.py
+++ books/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Book
 
 # Create your views here.
 def book_list(request):
-	return render(request, 'books/book_list.html', {})
+	books = Book.objects.order_by('start_date')
+	return render(request, 'books/book_list.html', {'books':books})
