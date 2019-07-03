3d53fd05cce0e81e6884b753454f461c7c8021f2
j-max <jmax.barry@gmail.com>
2019-07-03 17:06:44 -0500

    added book detail templates and initial css.

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Book
 
@@ -6,3 +6,7 @@ from .models import Book
 def book_list(request):
 	books = Book.objects.order_by('start_date')
 	return render(request, 'books/book_list.html', {'books':books})
+
+def book_detail(request, pk):
+	book = get_object_or_404(Book, pk=pk)
+	return render(request, 'books/book_detail.html', {'book': book})
